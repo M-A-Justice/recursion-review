482cf9ffa66a1a627fe1ebc2728edb92bbd54944
Evan <schafer.evan.r@gmail.com>
2020-03-23 16:02:53 -0700

    Complete getElementById
    get elementById passes the tests given through specRunner

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  let output = [];
+
+  var element = arguments[1] || document.body;
+
+  if (element.classList && element.classList.contains(className)) {
+    output.push(element);
+  }
+
+  element.childNodes.forEach(function(currentNode) {
+    output = output.concat(getElementsByClassName(className, currentNode));
+  })
+
+  return output;
 };
