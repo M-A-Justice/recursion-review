0ffe56c7612440bab1c20e71eeecd2d028240a73
Evan <schafer.evan.r@gmail.com>
2020-03-23 13:03:39 -0700

    Make progess on stringifyJSON
    passed tests for strings, numbers, null, arrays. Need to pass tests
    for objects.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,37 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  /*
+    - case for arrays, objects, string, and everything else
+  */
+  var arr = [];
+  let output = ``;
+  if (obj === null || typeof obj === 'number' || typeof obj === 'boolean') {
+    return `${obj}`;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    // if (obj.length === 0) {
+    //   return ``;
+    // }
+    // // output += `${obj[0]},`;
+    // // stringifyJSON(obj.slice(0));
+    // // // return '[' + stringifyJSON(obj) + ']';
+    obj.forEach(function(element) {
+      arr.push(stringifyJSON(element));
+    });
+    return `[${arr}]`;
+  } else if (typeof obj === 'object' && !Array.isArray(obj)) {
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        return '{}';
+      }
+    }
+    return '{' + obj + '}';
+  }
+  return output;
 };
+
+// var array = [1, 2, 3, 4, 5];
+
+// stringifyJSON(array);
