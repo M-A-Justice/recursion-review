7526206f8b13377dbb9573d1b4c2d8c7cd8940bc
Evan <schafer.evan.r@gmail.com>
2020-03-23 17:07:46 -0700

    Begin parseJson brainstorming
    parseJSON was begun, though no functionality was achieved.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,38 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  var array = [];
+  var object = {};
+  if (json.startsWith('[')) {
+    let jsonString = json.slice(0, json.length - 1);
+    let jsonArray = jsonString.split(',')
+    return array.push(parseJSON(jsonString));
+  }
+  if (json.startsWith('{')) {
+    let jsonString = json.slice(0, json.length - 1);
+
+  }
+
 };
+
+// '{"foo": "bar"}' i++ i+=2
+
+// !!"true"
+
+// Number()
+
+// play
+
+// ['"', 'p', 'l', 'a', 'y', '"']
+
+// ['"', '{', '}', '"']
+
+// {
+
+// charAt()
+
+/*
+  - possibly methods
+    - charAt()
+    -
+*/
